5d15bd4baa4c7c2aa6da27f8096c895653464653
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-06-08 14:43:07 +0200

    Rewrite and fix functions, reorder params, remove ref, add temporal radius

--- vsdehalo/denoise.py
+++ vsdehalo/denoise.py
@@ -1,22 +1,27 @@
 from __future__ import annotations
 
-from typing import Any, Dict, Literal, Sequence, Type
+from typing import Any, Dict, Literal, Sequence
 
 import vapoursynth as vs
+from vsdenoise import BM3D, BM3DCPU, BM3DCuda, BM3DCudaRTC
+from vsrgtools.util import norm_expr_planes, normalise_planes
 from vsutil import depth, disallow_variable_format, fallback, get_depth
 
 core = vs.core
 
 
 @disallow_variable_format
-def bidehalo(clip: vs.VideoNode, ref: vs.VideoNode | None = None,
-             sigma: float = 1.5, sigma_final: float | None = None,
-             radius: float = 5 / 255, radius_final: float | None = None,
-             cuda: bool | Literal['rtc'] = False,
-             planes: int | Sequence[int] | None = None,
-             bm3d_args: Dict[str, Any] = {},
-             bilateral_args: Dict[str, Any] = {},
-             ) -> vs.VideoNode:
+def bidehalo(
+    clip: vs.VideoNode,
+    sigma: float = 1.5, radius: float = 7,
+    sigma_final: float | None = None,
+    radius_final: float | None = None,
+    tr: int = 2, cuda: bool | Literal['rtc'] = False,
+    planes: int | Sequence[int] | None = None,
+    matrix: vs.MatrixCoefficients | int | None = None,
+    bm3d_args: Dict[str, Any] = {},
+    bilateral_args: Dict[str, Any] = {}
+) -> vs.VideoNode:
     """
     Simple dehalo function that uses ``bilateral`` and ``BM3D`` to remove bright haloing around edges.
 
@@ -27,7 +32,7 @@ def bidehalo(clip: vs.VideoNode, ref: vs.VideoNode | None = None,
     Both clips will be resampled to 16bit internally, and returned in the input bitdepth.
 
     Recommend values for `sigma` are between 0.8 and 2.0.
-    Recommend values for `radius` are between 5 / 255 and 15 / 255.
+    Recommend values for `radius` are between 5 and 15.
 
     Dependencies:
 
@@ -37,7 +42,6 @@ def bidehalo(clip: vs.VideoNode, ref: vs.VideoNode | None = None,
     * vsdenoise
 
     :param clip:                Clip to process.
-    :param ref:                 Reference clip.
     :param sigma:               ``Bilateral`` spatial weight sigma.
     :param sigma_final:         Final ``Bilateral`` call's spatial weight sigma.
                                 You'll want this to be much weaker than the initial `sigma`.
@@ -45,8 +49,11 @@ def bidehalo(clip: vs.VideoNode, ref: vs.VideoNode | None = None,
     :param radius:              ``Bilateral`` radius weight sigma.
     :param radius_final:        Final ``Bilateral`` radius weight sigma.
                                 if `None`, same as `radius`.
+    :param tr:                  Temporal radius for BM3D
     :param cuda:                Use ``BM3DCUDA`` and `BilateralGPU` if True, else ``BM3DCPU`` and `Bilateral`.
                                 Also accepts 'rtc' for ``BM3DRTC`` and `BilateralGPU_RTC`.
+                                Notice: final pass of bilateral will always be on cpu since the
+                                gpu implementation doesn't support passing ``ref``.
     :param planes:              Specifies which planes will be processed.
                                 Any unprocessed planes will be simply copied.
     :param bm3d_args:           Additional parameters to pass to BM3D.
@@ -54,53 +61,47 @@ def bidehalo(clip: vs.VideoNode, ref: vs.VideoNode | None = None,
 
     :return:                    Dehalo'd clip using ``BM3D`` and ``Bilateral``.
     """
-    try:
-        from vsdenoise import BM3DCPU, BM3DCuda, BM3DCudaRTC
-    except ModuleNotFoundError:
-        raise ModuleNotFoundError("bidehalo: 'Missing dependency `vsdenoise`!'")
-
     assert clip.format
-
-    if planes is None:
-        planes = list(range(clip.format.num_planes))
-    elif isinstance(planes, int):
-        planes = [planes]
 
     bits = get_depth(clip)
 
     sigma_final = fallback(sigma_final, sigma / 3)
     radius_final = fallback(radius_final, radius)
 
-    bm3d_in_args: Dict[str, Any] = dict(radius=2, planes=planes)
-    bm3d_func: Type[BM3DCPU] | Type[BM3DCuda] | Type[BM3DCudaRTC]
-    # bilateral_func: [WhatTypeGoesHere?]
+    planes = normalise_planes(clip, planes)
+
+    if matrix:
+        clip = clip.std.SetFrameProp('_Matrix', int(matrix))
+
+    process_chroma = 1 in planes or 2 in planes
+
+    if not cuda:
+        sigma_luma, sigma_chroma = 8, process_chroma and 6.4
+    else:
+        sigma_luma, sigma_chroma = 10, process_chroma and 8
+
+    bm3d_pargs = (depth(clip, 16), [sigma_luma, sigma_chroma], tr)
 
     if cuda is False:
-        bm3d_func = BM3DCPU
-        bilateral_func = core.bilateral.Bilateral
-        bm3d_in_args |= dict(sigma=10)
-    elif cuda is True:
-        bm3d_func = BM3DCuda
-        bilateral_func = core.bilateralgpu.Bilateral  # type:ignore[assignment]
-        bm3d_in_args |= dict(sigma=8)
-    elif cuda == 'rtc':
-        bm3d_func = BM3DCudaRTC
-        bilateral_func = core.bilateralgpu_rtc.Bilateral  # type:ignore[assignment]
-        bm3d_in_args |= dict(sigma=8)
+        try:
+            den = BM3D(*bm3d_pargs, **bm3d_args).clip
+        except AttributeError:
+            den = BM3DCPU(*bm3d_pargs, **bm3d_args).clip
+
+        ref = den.bilateral.Bilateral(None, sigma, radius / 255, planes, **bilateral_args)
     else:
-        raise ValueError(f"bidehalo: 'Invalid cuda selection ({cuda})!'")
+        bil_gpu_args: Dict[str, Any] = dict(sigma_spatial=sigma, **bilateral_args)
 
-    # Override with user's input settings
-    bm3d_in_args |= bm3d_args
+        if cuda is True:
+            den = BM3DCuda(*bm3d_pargs, **bm3d_args).clip
+            ref = den.bilateralgpu.Bilateral(**bil_gpu_args)
+        elif cuda == 'rtc':
+            den = BM3DCudaRTC(*bm3d_pargs, **bm3d_args).clip
+            ref = den.bilateralgpu_rtc.Bilateral(**bil_gpu_args)
+        else:
+            raise ValueError(f'bidehalo: Invalid cuda selection ({cuda})!')
 
-    if ref is None:
-        den = depth(bm3d_func(clip, **bm3d_in_args).clip, 16)
-
-        ref = bilateral_func(den, sigmaS=sigma, sigmaR=radius, planes=planes, **bilateral_args)
-    else:
-        ref = depth(ref, 16)
-
-    bidh = bilateral_func(clip, ref=ref, sigmaS=sigma_final, sigmaR=radius_final, planes=planes, **bilateral_args)
+    bidh = den.bilateral.Bilateral(ref, sigma_final, radius_final / 255, planes, **bilateral_args)
     bidh = depth(bidh, bits)
 
-    return core.std.Expr([clip, bidh], "x y min")
+    return core.std.Expr([clip, bidh], norm_expr_planes(clip, 'x y min', planes))
