706e24df8a118434d31319ef261f39b43e4d817d
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-10 18:37:23 +0200

    Add HQDeringmod

--- vsdehalo/denoise.py
+++ vsdehalo/denoise.py
@@ -1,11 +1,21 @@
 from __future__ import annotations
 
-from typing import Any, Dict, Literal
+from functools import partial
+from math import ceil
+from typing import Any, Dict, Literal, Tuple
 
 import vapoursynth as vs
-from vsdenoise import BM3D, BM3DCPU, BM3DCuda, BM3DCudaRTC
-from vsrgtools.util import PlanesT, norm_expr_planes, normalise_planes
-from vsutil import depth, disallow_variable_format, disallow_variable_resolution, fallback, get_depth
+from mvsfunc import LimitFilter  # type: ignore
+from vsdenoise import BM3D, BM3DCPU, BM3DCuda, BM3DCudaRTC, Prefilter, prefilter_clip
+from vsmask.edge import Prewitt
+from havsfunc import AvsPrewitt
+from vsmask.util import expand, inpand
+from vsrgtools import contrasharpening, contrasharpening_dehalo, removegrain, repair
+from vsrgtools.util import PlanesT, norm_expr_planes, normalise_planes, normalise_seq
+from vsutil import (
+    depth, disallow_variable_format, disallow_variable_resolution, fallback, get_depth, get_peak_value, iterate, join, scale_value,
+    split
+)
 
 core = vs.core
 
@@ -106,3 +116,178 @@ def bidehalo(
     bidh = depth(bidh, bits)
 
     return core.std.Expr([clip, bidh], norm_expr_planes(clip, 'x y min', planes))
+
+
+@disallow_variable_format
+@disallow_variable_resolution
+def HQDeringmod(
+    clip: vs.VideoNode,
+    smooth: vs.VideoNode | Prefilter | Tuple[Prefilter, Prefilter] | None = None,
+    ringmask: vs.VideoNode | None = None,
+    mrad: int = 1, msmooth: int = 1, minp: int = 1, mthr: int = 60, incedge: bool = False,
+    thr: float = 12.0, elast: float = 2.0, darkthr: float | None = None,
+    sigma: float = 128.0, sigma2: float | None = None,
+    sbsize: int | None = None, sosize: int | None = None,
+    contra: int | float | bool | None = None, drrep: int = 13,
+    planes: PlanesT = 0, show: bool = False, **kwargs: Any
+) -> vs.VideoNode:
+    """
+    :param clip:        Clip to process.
+    :param smooth:      Already smoothed clip, or a Prefilter, tuple for [luma, chroma] prefilter.
+    :param ringmask:    Custom ringing mask.
+    :param mrad:        Expanding iterations of edge mask, higher value means more aggressive processing.
+    :param msmooth:     Inflating iterations of edge mask, higher value means smoother edges of mask.
+    :param minp:        Inpanding iterations of prewitt edge mask, higher value means more aggressive processing.
+    :param mthr:        Threshold of prewitt edge mask, lower value means more aggressive processing
+                        but for strong ringing, lower value will treat some ringing as edge,
+                        which "protects" this ringing from being processed.
+    :param incedge:     Whether to include edge in ring mask, by default ring mask only include area near edges.
+    :param thr:         Threshold (8-bit scale) to limit filtering diff.
+                        Smaller thr will result in more pixels being taken from processed clip.
+                        Larger thr will result in less pixels being taken from input clip.
+                            PDiff: pixel value diff between processed clip and input clip
+                            ODiff: pixel value diff between output clip and input clip
+                            PDiff, thr and elast is used to calculate ODiff:
+                            ODiff = PDiff when [PDiff <= thr]
+                            ODiff gradually smooths from thr to 0 when [thr <= PDiff <= thr * elast]
+                            For elast>2.0, ODiff reaches maximum when [PDiff == thr * elast / 2]
+                            ODiff = 0 when [PDiff >= thr * elast]
+    :param elast:       Elasticity of the soft threshold.
+                        Larger "elast" will result in more pixels being blended from.
+    :param darkthr:     Threshold (8-bit scale) for darker area near edges, for filtering diff
+                        that brightening the image by default equals to thr/4.
+                        Set it lower if you think de-ringing destroys too much lines, etc.
+                        When darkthr is not equal to ``thr``, ``thr`` limits darkening,
+                        while ``darkthr`` limits brightening.
+                        This is useful to limit the overshoot/undershoot/blurring introduced in deringing.
+                        Examples:
+                            ``thr=0``,   ``darkthr=0``  : no limiting
+                            ``thr=255``, ``darkthr=255``: no limiting
+                            ``thr=8``,   ``darkthr=2``  : limit darkening with 8, brightening is limited to 2
+                            ``thr=8``,   ``darkthr=0``  : limit darkening with 8, brightening is limited to 0
+                            ``thr=255``, ``darkthr=0``  : limit darkening with 255, brightening is limited to 0
+                            For the last two examples, output will remain unchanged. (0/255: no limiting)
+    :param sigma:       DFTTest Prefilter only: sigma for medium frequecies
+    :param sigma2:      DFTTest Prefilter only: sigma for low&high frequecies
+    :param sbsize:      DFTTest Prefilter only: length of the sides of the spatial window
+    :param sosize:      DFTTest Prefilter only: spatial overlap amount
+    :param contra:      Whether to use contra-sharpening to resharp deringed clip:
+                            False: no contrasharpening
+                            True: auto radius for contrasharpening
+                            int 1-3: represents radius for contrasharpening
+                            float: represents level for contrasharpening_dehalo
+    :param drrep:       Use repair for details retention, recommended values are 13/12/1.
+    :param planes:      Planes to be processed.
+    :param show:        Show the computed ringing mask.
+    :param kwargs:      Kwargs to be passed to the prefilter function.
+
+    :return:            Deringed clip.
+    """
+    assert clip.format
+
+    if clip.format.color_family not in {vs.YUV, vs.GRAY}:
+        raise ValueError('HQDeringmod: format not supported')
+
+    peak = get_peak_value(clip)
+    bits = clip.format.bits_per_sample
+    planes = normalise_planes(clip, planes)
+    work_clip, *chroma = split(clip) if planes == [0] else (clip, )
+    assert work_clip.format
+
+    rep_4 = [4 if i in planes else 0 for i in range(work_clip.format.num_planes)]
+
+    is_HD = clip.width >= 1280 or clip.height >= 720
+
+    # Parameters for deringing kernel
+    sigma2 = fallback(sigma2, sigma / 16)
+    sbsize = fallback(sbsize, 8 if is_HD else 6)
+    sosize = fallback(sosize, 6 if is_HD else 4)
+
+    darkthr = fallback(darkthr, thr / 4)
+
+    if smooth is None:
+        smooth = Prefilter.MINBLUR2 if is_HD else Prefilter.MINBLUR1
+
+    rep_dr = [drrep if i in planes else 0 for i in range(work_clip.format.num_planes)]
+
+    # Kernel: Smoothing
+    if not isinstance(smooth, vs.VideoNode):
+        smoothy, smoothc = normalise_seq(smooth, 2)
+
+        def _get_kwargs(pref: Prefilter) -> Dict[str, Any]:
+            if pref != Prefilter.DFTTEST:
+                return kwargs
+            return kwargs | dict(
+                sbsize=sbsize, sosize=sosize, tbsize=1, slocation=[
+                    0.0, sigma2, 0.05, sigma, 0.5, sigma, 0.75, sigma2, 1.0, 0.0
+                ])
+
+        if smoothy == smoothc or work_clip.format.num_planes == 1:
+            smoothed = prefilter_clip(work_clip, smoothy, planes, **_get_kwargs(smoothy))
+        else:
+            smoothed = core.std.ShufflePlanes([
+                prefilter_clip(work_clip, smoothy, 0, **_get_kwargs(smoothy)),
+                prefilter_clip(work_clip, smoothc, list(set(planes) - {0}), **_get_kwargs(smoothc))
+            ], [0, 1, 2], vs.YUV)
+    else:
+        assert work_clip.format and smooth.format
+
+        if smooth.format.id != work_clip.format.id:
+            raise ValueError("Smooth clip format must match the source clip's!")
+
+        if smooth.width != work_clip.width or smooth.height != work_clip.height:
+            raise ValueError("Smooth clip sizes must match the source clip's!")
+
+        smoothed = smooth
+
+    # Post-Process: Contra-Sharpening
+    if contra:
+        if isinstance(contra, int):
+            smoothed = contrasharpening(smoothed, work_clip, contra, 13, planes)
+        else:
+            smoothed = contrasharpening_dehalo(smoothed, work_clip, contra)  # FIXME doesn't accept planes
+
+    # Post-Process: Repairing
+    if set(rep_dr) != {0}:
+        repclp = repair(work_clip, smoothed, drrep)
+    else:
+        repclp = work_clip
+
+    # Post-Process: Limiting
+    if not ((thr <= 0 and darkthr <= 0) or (thr >= 255 and darkthr >= 255)):
+        limitclp = LimitFilter(repclp, work_clip, thr=thr, elast=elast, brighten_thr=darkthr, planes=planes)
+    else:
+        limitclp = repclp
+
+    if ringmask is None:
+        # FIXME: <= instead of < for lthr, Vardë?
+        prewittm = Prewitt().edgemask(work_clip, scale_value(mthr, 8, bits) + 1)
+
+        fmask = prewittm.std.Median(planes).misc.Hysteresis(prewittm, planes)
+
+        omask = expand(fmask, mrad, mrad, planes=planes) if mrad > 0 else fmask
+
+        if msmooth > 0:
+            omask = iterate(omask, partial(core.std.Inflate, planes=planes), msmooth)
+
+        if incedge:
+            ringmask = omask
+        else:
+            if minp <= 0:
+                imask = fmask
+            elif minp % 2 == 0:
+                imask = inpand(fmask, minp // 2, planes=planes)
+            else:
+                imask = inpand(fmask.std.Inflate(planes), ceil(minp / 2), planes=planes)
+
+            ringmask = core.std.Expr([omask, imask], f'x {peak} y - * {peak} /')
+
+    dering = work_clip.std.MaskedMerge(limitclp, ringmask, planes)
+
+    if show:
+        return ringmask
+
+    if chroma:
+        return join([dering, *chroma], clip.format.color_family)
+
+    return dering
