12c210d8ce5afcc095e564d406a894a0ea6b0c9f
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-12 02:17:18 +0200

    masks: Add kwargs

--- vsdehalo/masks.py
+++ vsdehalo/masks.py
@@ -1,3 +1,4 @@
+from typing import Any
 import vapoursynth as vs
 from vsmask.edge import TriticalTCanny
 from vsrgtools.util import PlanesT, iterate, norm_expr_planes
@@ -25,41 +26,41 @@ class TritSigmaTCanny(TriticalTCanny):
 
 @disallow_variable_format
 @disallow_variable_resolution
-def dilation(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None) -> vs.VideoNode:
+def dilation(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None, **kwargs: Any) -> vs.VideoNode:
     if radius < 1:
         raise RuntimeError('mask.dilation: radius has to be greater than 0!')
 
-    return iterate(src, core.std.Maximum, radius, planes)
+    return iterate(src, core.std.Maximum, radius, planes, **kwargs)
 
 
 @disallow_variable_format
 @disallow_variable_resolution
-def erosion(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None) -> vs.VideoNode:
+def erosion(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None, **kwargs: Any) -> vs.VideoNode:
     if radius < 1:
         raise RuntimeError('mask.erosion: radius has to be greater than 0!')
 
-    return iterate(src, core.std.Minimum, radius, planes)
+    return iterate(src, core.std.Minimum, radius, planes, **kwargs)
 
 
 @disallow_variable_format
 @disallow_variable_resolution
-def closing(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None) -> vs.VideoNode:
+def closing(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None, **kwargs: Any) -> vs.VideoNode:
     if radius < 1:
         raise RuntimeError('mask.closing: radius has to be greater than 0!')
 
-    dilated = dilation(src, radius, planes)
-    eroded = erosion(dilated, radius, planes)
+    dilated = dilation(src, radius, planes, **kwargs)
+    eroded = erosion(dilated, radius, planes, **kwargs)
 
     return eroded
 
 
 @disallow_variable_format
 @disallow_variable_resolution
-def opening(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None) -> vs.VideoNode:
+def opening(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None, **kwargs: Any) -> vs.VideoNode:
     if radius < 1:
         raise RuntimeError('mask.opening: radius has to be greater than 0!')
 
-    eroded = erosion(src, radius, planes)
+    eroded = erosion(src, radius, planes, **kwargs)
     dilated = dilation(eroded, radius, planes)
 
     return dilated
@@ -67,33 +68,33 @@ def opening(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None) -> vs.Vi
 
 @disallow_variable_format
 @disallow_variable_resolution
-def gradient(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None) -> vs.VideoNode:
+def gradient(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None, **kwargs: Any) -> vs.VideoNode:
     if radius < 1:
         raise RuntimeError('mask.gradient: radius has to be greater than 0!')
 
     eroded = erosion(src, radius, planes)
-    dilated = dilation(src, radius, planes)
+    dilated = dilation(src, radius, planes, **kwargs)
 
     return core.std.Expr([dilated, eroded], norm_expr_planes(src, 'x y -', planes))
 
 
 @disallow_variable_format
 @disallow_variable_resolution
-def top_hat(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None) -> vs.VideoNode:
+def top_hat(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None, **kwargs: Any) -> vs.VideoNode:
     if radius < 1:
         raise RuntimeError('mask.top_hat: radius has to be greater than 0!')
 
-    opened = opening(src, radius, planes)
+    opened = opening(src, radius, planes, **kwargs)
 
     return core.std.Expr([src, opened], norm_expr_planes(src, 'x y -', planes))
 
 
 @disallow_variable_format
 @disallow_variable_resolution
-def black_hat(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None) -> vs.VideoNode:
+def black_hat(src: vs.VideoNode, radius: int = 1, planes: PlanesT = None, **kwargs: Any) -> vs.VideoNode:
     if radius < 1:
         raise RuntimeError('mask.black_hat: radius has to be greater than 0!')
 
-    closed = closing(src, radius, planes)
+    closed = closing(src, radius, planes, **kwargs)
 
     return core.std.Expr([closed, src], norm_expr_planes(src, 'x y -', planes))
