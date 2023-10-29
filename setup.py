4d6f657bff8429588dac4be9748ba35075e6a799
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-10-29 18:29:50 +0100

    Change ownership

--- setup.py
+++ setup.py
@@ -22,9 +22,8 @@ setuptools.setup(
     long_description=readme,
     long_description_content_type='text/markdown',
     project_urls={
-        'Source Code': 'https://github.com/Irrational-Encoding-Wizardry/vs-dehalo',
-        'Documentation': 'https://vsdehalo.encode.moe/en/latest/',
-        'Contact': 'https://discord.gg/qxTxVJGtst',
+        'Source Code': 'https://github.com/Setsugennoao/vs-dehalo',
+        'Contact': 'https://discord.gg/setsugen',
     },
     install_requires=requirements,
     python_requires='>=3.11',
