d0d6acd7c0378d92142caf8a5c5616173a3eb3f7
arch1t3cht <arch1t3cht@gmail.com>
2025-02-13 15:30:01 +0100

    Final version bump to show deprecation warning

--- vsdehalo/_metadata.py
+++ vsdehalo/_metadata.py
@@ -1,6 +1,6 @@
 """Collection of dehaloing VapourSynth functions"""
 
-__version__ = '1.10.1'
+__version__ = '1.10.2'
 
 __author_name__, __author_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'
 __maintainer_name__, __maintainer_email__ = __author_name__, __author_email__
